123883f452595253a749a29ff7ec70ea6d6a397a
minhduc1706 <135154288+minhduc1706@users.noreply.github.com>
2025-03-28 19:13:14 +0700

    Update useService.ts

--- src/queries/useService.ts
+++ src/queries/useService.ts
@@ -1,5 +1,5 @@
 import { getService } from "@/api/service";
-import { Service } from "@/interfaces/service";
+import { Service } from "@/interfaces/Service";
 import { useQuery } from "@tanstack/react-query";
 
 
